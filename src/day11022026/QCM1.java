130d6eff2089841f08d6ba673727172b97f93e02
Mohamed Amine MEZGHICH <git@mezghich.com>
2026-02-11 21:59:26 +0100

    qcm 1 & collections

---
+++ src/day11022026/QCM1.java
@@ -0,0 +1,34 @@
+package day11022026;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class QCM1 {
+
+	public static void main(String[] args) {
+		/*
+		IntStream is = IntStream.empty();
+		int res = is.sum();
+		System.out.println(res);
+		
+		System.out.println(Stream.iterate(1, x -> ++x)
+	      .limit(5)
+	      .map(x -> x+"")
+	      .collect(Collectors.joining()));
+         */
+		List<Integer> l1 = Arrays.asList(1, 2, 3);
+		List<Integer> l2 = Arrays.asList(4, 5, 6);
+		List<Integer> l3 = Arrays.asList();
+
+		Stream.of(l1, l2, l3)
+		      .flatMap(x -> x.stream())
+		      .map(x -> x + 1)
+		      .forEach(System.out::print);
+
+		
+	}
+
+}
